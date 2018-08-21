a33aaf92231b6dabf05641072fb6cd7996f67405
Raditya Kottama <kottamaraditya@yahoo.com>
2018-08-21 14:16:48 +0700

    no message

---
+++ MC 3/InstructionSCENEViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InstructionSCENEViewController.swift
+//  MC 3
+//
+//  Created by Raditya Kottama on 21/08/18.
+//  Copyright © 2018 Raditya Kottama. All rights reserved.
+//
+
+import UIKit
+
+class InstructionSCENEViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
