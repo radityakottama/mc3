c646fbc2b66cb39d838d71a6b38477be10a7828d
Klaudius Ivan <klaudsvanz@gmail.com>
2018-08-28 16:49:41 +0700

    no message

--- MC 3/TrainBank.swift
+++ MC 3/TrainBank.swift
@@ -24,18 +24,18 @@ class TrainBank {
     static func obedienceTrainData() -> [TrainBank]
     {
         return [
-            TrainBank(titletrain: "Duduk", trainImage: [UIImage(named: "sit_button")!,UIImage(named: "sit_button_clicked")!,UIImage(named: "guide_2")!]),
-            TrainBank(titletrain: "Berbaring", trainImage: [UIImage(named: "down_button")!,UIImage(named: "down_button_clicked")!,UIImage(named: "guide_1")!]),
-            TrainBank(titletrain: "Datang", trainImage: [UIImage(named: "come_button")!, UIImage(named: "come_button_clicked")!]),
-            TrainBank(titletrain: "Diam", trainImage: [UIImage(named: "stay_button")!, UIImage(named: "stay_button_clicked")!])
+            TrainBank(titletrain: "Duduk", trainImage: [UIImage(named: "sit_button_2.0")!,UIImage(named: "guide_2")!]),
+            TrainBank(titletrain: "Berbaring", trainImage: [UIImage(named: "down_button_2.0")!,UIImage(named: "guide_1")!]),
+            TrainBank(titletrain: "Datang", trainImage: [UIImage(named: "come_button_2.0")!]),
+            TrainBank(titletrain: "Diam", trainImage: [UIImage(named: "stay_button_2.0")!])
         ]
     }
     static func behavioralTrainData() -> [TrainBank]
     {
         return [
-            TrainBank(titletrain: "Menggongong Berlebihan", trainImage: [UIImage(named: "bark_button")!, UIImage(named: "bark_button_clicked")!]),
-            TrainBank(titletrain: "Mengunyah", trainImage: [UIImage(named: "chewing_button")!, UIImage(named: "chewing_button_clicked")!]),
-            TrainBank(titletrain: "Memberantakan Rumah", trainImage: [UIImage(named: "breaking_rule_button")!, UIImage(named: "breaking_rule_button_clicked")!])
+            TrainBank(titletrain: "Menggongong Berlebihan", trainImage: [UIImage(named: "bark_button_2.0")!]),
+            TrainBank(titletrain: "Mengunyah", trainImage: [UIImage(named: "chewing_button_2.0")!]),
+            TrainBank(titletrain: "Memberantakan Rumah", trainImage: [UIImage(named: "breaking_rule_button_2.0")!])
         ]
     }
 
